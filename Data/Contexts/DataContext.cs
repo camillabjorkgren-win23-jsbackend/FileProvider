a76192b8b8fd5c0a4f0224a78cf18dd8d7e30e29
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2024-05-18 11:51:29 +0200

    Add project files.

---
+++ Data/Contexts/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Contexts;
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+    public DbSet<FileEntity> Files { get; set; }
+}
